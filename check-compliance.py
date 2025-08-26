e1e8fab18495915431a5cd790373e0ea074bb450
su3inni <139996611+su3inni@users.noreply.github.com>
2025-08-26 17:21:55 +0900

    lib version

--- check-compliance.py
+++ check-compliance.py
@@ -1,3 +1,4 @@
+# python 3.9 azure-identity==1.13.0 \ azure-mgmt-compute==29.1.0 \ azure-mgmt-resource==21.1.0
 #!/usr/bin/env python3
 import json, requests, hashlib, hmac, base64, datetime
 from azure.identity import DefaultAzureCredential
@@ -66,48 +67,45 @@ def find_resources_using_des(resource_client, des_id):
 def run():
     cred = DefaultAzureCredential()
     for sub_id in WORKLOAD_SUBSCRIPTIONS:
-        # workload subscription's compute / all resource
         compute = ComputeManagementClient(cred, sub_id)
         resource = ResourceManagementClient(cred, sub_id)
 
-        # des in workload subscription
-        for des in compute.disk_encryption_sets.list_by_subscription():
-            des_name = des.name
-            des_subcode, des_wid = extract_subcode_workload(des_name)
+        for rg in resource.resource_groups.list():
+            for des in compute.disk_encryption_sets.list(rg.name):
+                des_name = des.name
+                des_subcode, des_wid = extract_subcode_workload(des_name)
 
-            # uami w/des
-            uamis = list(des.identity.user_assigned_identities.keys()) if (
-                des.identity and des.identity.user_assigned_identities) else []
-            # UAMI 없으면 다음
-            if len(uamis)==0 : break 
+                uamis = list(des.identity.user_assigned_identities.keys()) if (
+                    des.identity and des.identity.user_assigned_identities) else []
+                if len(uamis) == 0:
+                    continue
 
-            # linked resource w/des 
-            linked_resources = find_resources_using_des(resource, des.id)
+                linked_resources = find_resources_using_des(resource, des.id)
 
-            for uami_id in uamis :  
-                if uami_id:
-                    uname = uami_id.split("/")[-1]
-                    _, uwid = extract_subcode_workload(uname)
-                    uami_mapping_valid = (uwid == des_wid)
+                for uami_id in uamis:
+                    if uami_id:
+                        uname = uami_id.split("/")[-1]
+                        _, uwid = extract_subcode_workload(uname)
+                        uami_mapping_valid = (uwid == des_wid)
 
-                for res in linked_resources or [None]:  # 리소스 없으면 None 처리
-                    if res:
-                        rsub, rwid = extract_subcode_workload(res['name'])
-                        resource_mapping_valid = (rsub == des_subcode and rwid == des_wid)
-                        resource_id = f"/subscriptions/{sub_id}/resourceGroups/{res['resourceGroup']}/providers/{res['type']}/{res['name']}"
-                    else:
-                        resource_mapping_valid = None
-                        resource_id = None
+                    for res in linked_resources or [None]:
+                        if res:
+                            rsub, rwid = extract_subcode_workload(res['name'])
+                            resource_mapping_valid = (rsub == des_subcode and rwid == des_wid)
+                            resource_id = f"/subscriptions/{sub_id}/resourceGroups/{res['resourceGroup']}/providers/{res['type']}/{res['name']}"
+                        else:
+                            resource_mapping_valid = None
+                            resource_id = None
 
-                    log_data = {
-                        "timeGenerated": datetime.datetime.utcnow().isoformat() + "Z",
-                        "subscriptionId": sub_id,
-                        "desName": des_name,
-                        "uamiId": uami_id,
-                        "uamiMappingValid": uami_mapping_valid,
-                        "resourceId": resource_id,
-                        "resourceMappingValid": resource_mapping_valid
-                    }
-                    send_log(log_data)
+                        log_data = {
+                            "timeGenerated": datetime.datetime.utcnow().isoformat() + "Z",
+                            "subscriptionId": sub_id,
+                            "desName": des_name,
+                            "uamiId": uami_id,
+                            "uamiMappingValid": uami_mapping_valid,
+                            "resourceId": resource_id,
+                            "resourceMappingValid": resource_mapping_valid
+                        }
+                        send_log(log_data)
 
 run()
